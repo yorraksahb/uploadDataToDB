b6a6dfde6e69d452d49ac085fa7a38e9a662fc1e
Bhaskar Roy <bhaskar.x.roy@gmail.com>
2017-03-03 11:54:57 +0530

    Removing Comments

--- handler.js
+++ handler.js
@@ -2,31 +2,16 @@
 var enrichBO = require('./deviceEnrichDelgate.js'),
 
     handler = function(event, context, callback) {
-        console.log(JSON.stringify(event));
         var env = event.table;
-        var httpMethod = event.httpmethod;
-        var path = event.path;
         console.log('Event: ' + JSON.stringify(event));
 
         enrichBO.startOps(env, function(err) {
             if (err) {
-                return callback(err);
+                return callback(JSON.stringify(err));
             } else {
                 return callback('Success');
             }
         });
-
-        /*if (httpMethod === 'GET' && path === '/enricher') {
-            enrichBO.startOps(env, function(err) {
-                if (err) {
-                    return callback(JSON.stringify(err));
-                } else {
-                    return callback('Success');
-                }
-            });
-        } else {
-            return callback('Error starting the function');
-        } */
     };
 
 module.exports = {
